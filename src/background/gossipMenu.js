6b28da1c1adddbaa7f8dd17b027f715972dd3aa7
CalsRanna <calsranna@gmail.com>
2021-01-14 15:45:00 +0800

    Build gossip menu module.

--- src/background/gossipMenu.js
+++ src/background/gossipMenu.js
@@ -1,15 +1,24 @@
 import { ipcMain } from "electron";
-import { COUNT_GOSSIP_MENUS, GLOBAL_NOTICE, SEARCH_GOSSIP_MENUS } from "../constants";
+import {
+  COPY_GOSSIP_MENU,
+  COUNT_GOSSIP_MENUS,
+  CREATE_GOSSIP_MENU,
+  DESTROY_GOSSIP_MENU,
+  FIND_GOSSIP_MENU,
+  GLOBAL_NOTICE,
+  SEARCH_GOSSIP_MENUS,
+  STORE_GOSSIP_MENU,
+  UPDATE_GOSSIP_MENU,
+} from "../constants";
 
 const { knex } = require("../libs/mysql");
 
-// 搜索满足条件的生物模板
 ipcMain.on(SEARCH_GOSSIP_MENUS, (event, payload) => {
   let queryBuilder = knex()
     .select(["gm.*", "nt.text0_0", "nt.text0_1", "ntl.Text0_0", "ntl.Text0_1"])
     .from("gossip_menu as gm")
     .leftJoin("npc_text as nt", "gm.TextID", "nt.ID")
-    .leftJoin("npc_text_locale as ntl", function() {
+    .leftJoin("npc_text_locale as ntl", function () {
       this.on("gm.TextID", "=", "ntl.ID").andOn("ntl.Locale", "=", knex().raw("?", "zhCN"));
     });
   if (payload.MenuID) {
@@ -23,22 +32,21 @@ ipcMain.on(SEARCH_GOSSIP_MENUS, (event, payload) => {
   }
   queryBuilder = queryBuilder.limit(50).offset(payload.page != undefined ? (payload.page - 1) * 50 : 0);
 
-  queryBuilder.then(rows => {
+  queryBuilder.then((rows) => {
     event.reply(SEARCH_GOSSIP_MENUS, rows);
     event.reply(GLOBAL_NOTICE, {
       category: "message",
-      message: queryBuilder.toString()
+      message: queryBuilder.toString(),
     });
   });
 });
 
-// 计算满足条件的生物模板数量
 ipcMain.on(COUNT_GOSSIP_MENUS, (event, payload) => {
   let queryBuilder = knex()
     .count("* as total")
     .from("gossip_menu as gm")
     .leftJoin("npc_text as nt", "gm.TextID", "nt.ID")
-    .leftJoin("npc_text_locale as ntl", function() {
+    .leftJoin("npc_text_locale as ntl", function () {
       this.on("gm.TextID", "=", "ntl.ID").andOn("ntl.Locale", "=", knex().raw("?", "zhCN"));
     });
   if (payload.MenuID) {
@@ -51,11 +59,140 @@ ipcMain.on(COUNT_GOSSIP_MENUS, (event, payload) => {
     );
   }
 
-  queryBuilder.then(rows => {
+  queryBuilder.then((rows) => {
     event.reply(COUNT_GOSSIP_MENUS, rows[0].total);
     event.reply(GLOBAL_NOTICE, {
       category: "message",
+      message: queryBuilder.toString(),
+    });
+  });
+});
+
+ipcMain.on(STORE_GOSSIP_MENU, (event, payload) => {
+  let queryBuilder = knex().insert(payload).into("gossip_menu");
+
+  queryBuilder.then((rows) => {
+    event.reply(COUNT_GOSSIP_MENUS, rows);
+    event.reply(GLOBAL_NOTICE, {
+      category: "notification",
+      title: "成功",
+      message: "新建成功。",
+      type: "success",
+    });
+    event.reply(GLOBAL_NOTICE, {
+      category: "message",
+      message: queryBuilder.toString(),
+    });
+  });
+});
+
+ipcMain.on(FIND_GOSSIP_MENU, (event, payload) => {
+  let queryBuilder = knex().select().from("gossip_menu").where(payload);
+
+  queryBuilder.then((rows) => {
+    event.reply(FIND_GOSSIP_MENU, rows.length > 0 ? rows[0] : {});
+    event.reply(GLOBAL_NOTICE, {
+      category: "message",
+      message: queryBuilder.toString(),
+    });
+  });
+});
+
+ipcMain.on(UPDATE_GOSSIP_MENU, (event, payload) => {
+  let queryBuilder = knex()
+    .table("gossip_menu")
+    .where("MenuID", payload.credential.MenuID)
+    .update(payload.gossipMenu);
+
+  queryBuilder.then(rows => {
+    event.reply(UPDATE_GOSSIP_MENU, rows);
+    event.reply(GLOBAL_NOTICE, {
+      category: "notification",
+      title: "成功",
+      message: "修改成功。",
+      type: "success"
+    });
+    event.reply(GLOBAL_NOTICE, {
+      category: "message",
       message: queryBuilder.toString()
+    });
+  });
+});
+
+ipcMain.on(DESTROY_GOSSIP_MENU, (event, payload) => {
+  let queryBuilder = knex()
+    .table("gossip_menu")
+    .where(payload)
+    .delete();
+
+  queryBuilder.then(rows => {
+    event.reply(DESTROY_GOSSIP_MENU, rows);
+    event.reply("GLOBAL_NOTICE", {
+      category: "notification",
+      title: "成功",
+      message: "删除成功。",
+      type: "success"
+    });
+    event.reply(GLOBAL_NOTICE, {
+      category: "message",
+      message: queryBuilder.toString()
+    });
+  });
+});
+
+ipcMain.on(CREATE_GOSSIP_MENU, (event, payload) => {
+  let queryBuilder = knex()
+    .select("MenuID")
+    .from("gossip_menu")
+    .orderBy("MenuID", "desc");
+
+  queryBuilder.then(rows => {
+    event.reply(CREATE_GOSSIP_MENU, {
+      MenuID: rows[0].MenuID + 1
+    });
+    event.reply(GLOBAL_NOTICE, {
+      category: "message",
+      message: queryBuilder.toString()
+    });
+  });
+});
+
+ipcMain.on(COPY_GOSSIP_MENU, (event, payload) => {
+  let MenuID = undefined;
+  let gossipMenu = undefined;
+
+  let menuIdQueryBuilder = knex()
+    .select("MenuID")
+    .from("gossip_menu")
+    .orderBy("MenuID", "desc");
+  let findGossipMenuQueryBuilder = knex()
+    .select()
+    .from("gossip_menu")
+    .where(payload);
+  Promise.all([
+    menuIdQueryBuilder.then(rows => {
+      MenuID = rows[0].MenuID;
+    }),
+    findGossipMenuQueryBuilder.then(rows => {
+      gossipMenu = rows.length > 0 ? rows[0] : {};
+    })
+  ]).then(() => {
+    gossipMenu.MenuID = MenuID + 1;
+    let queryBuilder = knex()
+      .insert(gossipMenu)
+      .into("gossip_menu");
+    queryBuilder.then(rows => {
+      event.reply(COPY_GOSSIP_MENU, rows);
+      event.reply(GLOBAL_NOTICE, {
+        type: "success",
+        category: "notification",
+        title: "成功",
+        message: `复制成功，新的对话 MenuID 为 ${MenuID + 1}。`
+      });
+      event.reply(GLOBAL_NOTICE, {
+        category: "message",
+        message: queryBuilder.toString()
+      });
     });
   });
 });
--- src/store/gossipMenu.js
+++ src/store/gossipMenu.js
@@ -1,6 +1,16 @@
 const ipcRenderer = window.require("electron").ipcRenderer;
 
-import { SEARCH_GOSSIP_MENUS, COUNT_GOSSIP_MENUS, PAGINATE_GOSSIP_MENUS } from "../constants";
+import {
+  COPY_GOSSIP_MENU,
+  COUNT_GOSSIP_MENUS,
+  CREATE_GOSSIP_MENU,
+  DESTROY_GOSSIP_MENU,
+  FIND_GOSSIP_MENU,
+  PAGINATE_GOSSIP_MENUS,
+  SEARCH_GOSSIP_MENUS,
+  STORE_GOSSIP_MENU,
+  UPDATE_GOSSIP_MENU,
+} from "../constants";
 
 export default {
   namespaced: true,
@@ -37,7 +47,58 @@ export default {
         commit(PAGINATE_GOSSIP_MENUS, payload.page);
         resolve();
       });
-    }
+    },
+    storeGossipMenu(context, payload) {
+      return new Promise((resolve) => {
+        ipcRenderer.send(STORE_GOSSIP_MENU, payload);
+        ipcRenderer.on(STORE_GOSSIP_MENU, () => {
+          resolve();
+        });
+      });
+    },
+    findGossipMenu({ commit }, payload) {
+      return new Promise((resolve) => {
+        ipcRenderer.send(FIND_GOSSIP_MENU, payload);
+        ipcRenderer.on(FIND_GOSSIP_MENU, (event, response) => {
+          commit(FIND_GOSSIP_MENU, response);
+          resolve();
+        });
+      });
+    },
+    updateGossipMenu({ commit }, payload) {
+      return new Promise((resolve) => {
+        ipcRenderer.send(UPDATE_GOSSIP_MENU, payload);
+        ipcRenderer.on(UPDATE_GOSSIP_MENU, () => {
+          commit(UPDATE_GOSSIP_MENU, payload.gossipMenu);
+          resolve();
+        });
+      });
+    },
+    destroyGossipMenu(context, payload) {
+      return new Promise((resolve) => {
+        ipcRenderer.send(DESTROY_GOSSIP_MENU, payload);
+        ipcRenderer.on(DESTROY_GOSSIP_MENU, () => {
+          resolve();
+        });
+      });
+    },
+    createGossipMenu({ commit }, payload) {
+      return new Promise((resolve) => {
+        ipcRenderer.send(CREATE_GOSSIP_MENU, payload);
+        ipcRenderer.on(CREATE_GOSSIP_MENU, (event, response) => {
+          commit(CREATE_GOSSIP_MENU, response);
+          resolve();
+        });
+      });
+    },
+    copyGossipMenu(context, payload) {
+      return new Promise((resolve) => {
+        ipcRenderer.send(COPY_GOSSIP_MENU, payload);
+        ipcRenderer.on(COPY_GOSSIP_MENU, () => {
+          resolve();
+        });
+      });
+    },
   },
   mutations: {
     [SEARCH_GOSSIP_MENUS](state, gossipMenus) {
@@ -48,6 +109,15 @@ export default {
     },
     [PAGINATE_GOSSIP_MENUS](state, page) {
       state.page = page;
-    }
+    },
+    [FIND_GOSSIP_MENU](state, gossipMenu) {
+      state.gossipMenu = gossipMenu;
+    },
+    [UPDATE_GOSSIP_MENU](state, gossipMenu) {
+      state.gossipMenu = gossipMenu;
+    },
+    [CREATE_GOSSIP_MENU](state, gossipMenu) {
+      state.gossipMenu = gossipMenu;
+    },
   }
 };
