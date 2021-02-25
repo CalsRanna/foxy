083641102eb9eea60ae2b9863f280afb4aee1172
CalsRanna <calsranna@gmail.com>
2021-02-25 12:50:26 +0800

    Feature: add quest template selector.

---
+++ src/background/questTemplateSelector.js
@@ -0,0 +1,91 @@
+import { ipcMain } from "electron";
+
+import {
+  SEARCH_QUEST_TEMPLATES_FOR_SELECTOR,
+  COUNT_QUEST_TEMPLATES_FOR_SELECTOR,
+  GLOBAL_MESSAGE_BOX,
+  GLOBAL_MESSAGE,
+} from "../constants";
+
+const { knex } = require("../libs/mysql");
+
+ipcMain.on(SEARCH_QUEST_TEMPLATES_FOR_SELECTOR, (event, payload) => {
+  let queryBuilder = knex()
+    .select([
+      "qt.ID",
+      "qt.LogTitle",
+      "qtl.Title",
+      "qt.LogDescription",
+      "qtl.Details",
+      "qt.QuestLevel",
+      "qt.MinLevel",
+    ])
+    .from("quest_template as qt")
+    .leftJoin("quest_template_locale as qtl", function() {
+      this.on("qt.ID", "=", "qtl.ID").andOn(
+        "qtl.locale",
+        "=",
+        knex().raw("?", "zhCN")
+      );
+    });
+  if (payload.id) {
+    queryBuilder = queryBuilder.where("qt.ID", "like", `%${payload.id}%`);
+  }
+  if (payload.title) {
+    queryBuilder = queryBuilder.where((builder) =>
+      builder
+        .where("qt.LogTitle", "like", `%${payload.title}%`)
+        .orWhere("qtl.Title", "like", `%${payload.title}%`)
+    );
+  }
+  queryBuilder = queryBuilder
+    .limit(50)
+    .offset(payload.page != undefined ? (payload.page - 1) * 50 : 0);
+
+  queryBuilder
+    .then((rows) => {
+      event.reply(SEARCH_QUEST_TEMPLATES_FOR_SELECTOR, rows);
+    })
+    .catch((error) => {
+      event.reply(`${SEARCH_QUEST_TEMPLATES_FOR_SELECTOR}_REJECT`, error);
+      event.reply(GLOBAL_MESSAGE_BOX, JSON.stringify(error));
+    })
+    .finally(() => {
+      event.reply(GLOBAL_MESSAGE, queryBuilder.toString());
+    });
+});
+
+ipcMain.on(COUNT_QUEST_TEMPLATES_FOR_SELECTOR, (event, payload) => {
+  let queryBuilder = knex()
+    .count("* as total")
+    .from("quest_template as qt")
+    .leftJoin("quest_template_locale as qtl", function() {
+      this.on("qt.ID", "=", "qtl.ID").andOn(
+        "qtl.locale",
+        "=",
+        knex().raw("?", "zhCN")
+      );
+    });
+  if (payload.id) {
+    queryBuilder = queryBuilder.where("qt.ID", "like", `%${payload.id}%`);
+  }
+  if (payload.title) {
+    queryBuilder = queryBuilder.where((builder) =>
+      builder
+        .where("qt.LogTitle", "like", `%${payload.title}%`)
+        .orWhere("qtl.Title", "like", `%${payload.title}%`)
+    );
+  }
+
+  queryBuilder
+    .then((rows) => {
+      event.reply(COUNT_QUEST_TEMPLATES_FOR_SELECTOR, rows[0].total);
+    })
+    .catch((error) => {
+      event.reply(`${COUNT_QUEST_TEMPLATES_FOR_SELECTOR}_REJECT`, error);
+      event.reply(GLOBAL_MESSAGE_BOX, JSON.stringify(error));
+    })
+    .finally(() => {
+      event.reply(GLOBAL_MESSAGE, queryBuilder.toString());
+    });
+});
---
+++ src/store/questTemplateSelector.js
@@ -0,0 +1,74 @@
+const ipcRenderer = window.require("electron").ipcRenderer;
+
+import {
+  SEARCH_QUEST_TEMPLATES_FOR_SELECTOR,
+  COUNT_QUEST_TEMPLATES_FOR_SELECTOR,
+  PAGINATE_QUEST_TEMPLATES_FOR_SELECTOR,
+} from "../constants";
+
+export default {
+  namespaced: true,
+  state: () => ({
+    pagination: {
+      total: 0,
+      page: 1,
+      size: 50,
+    },
+    questTemplates: [],
+  }),
+  actions: {
+    searchQuestTemplatesForSelector({ commit }, payload) {
+      return new Promise((resolve, reject) => {
+        ipcRenderer.send(SEARCH_QUEST_TEMPLATES_FOR_SELECTOR, payload);
+        ipcRenderer.on(
+          SEARCH_QUEST_TEMPLATES_FOR_SELECTOR,
+          (event, response) => {
+            commit(SEARCH_QUEST_TEMPLATES_FOR_SELECTOR, response);
+            resolve();
+          }
+        );
+        ipcRenderer.on(
+          `${SEARCH_QUEST_TEMPLATES_FOR_SELECTOR}_REJECT`,
+          (event, error) => {
+            reject(error);
+          }
+        );
+      });
+    },
+    countQuestTemplatesForSelector({ commit }, payload) {
+      return new Promise((resolve, reject) => {
+        ipcRenderer.send(COUNT_QUEST_TEMPLATES_FOR_SELECTOR, payload);
+        ipcRenderer.on(
+          COUNT_QUEST_TEMPLATES_FOR_SELECTOR,
+          (event, response) => {
+            commit(COUNT_QUEST_TEMPLATES_FOR_SELECTOR, response);
+            resolve();
+          }
+        );
+        ipcRenderer.on(
+          `${COUNT_QUEST_TEMPLATES_FOR_SELECTOR}_REJECT`,
+          (event, error) => {
+            reject(error);
+          }
+        );
+      });
+    },
+    paginateQuestTemplatesForSelector({ commit }, payload) {
+      return new Promise((resolve) => {
+        commit(PAGINATE_QUEST_TEMPLATES_FOR_SELECTOR, payload.page);
+        resolve();
+      });
+    },
+  },
+  mutations: {
+    [SEARCH_QUEST_TEMPLATES_FOR_SELECTOR](state, questTemplates) {
+      state.questTemplates = questTemplates;
+    },
+    [COUNT_QUEST_TEMPLATES_FOR_SELECTOR](state, total) {
+      state.pagination.total = total;
+    },
+    [PAGINATE_QUEST_TEMPLATES_FOR_SELECTOR](state, page) {
+      state.pagination.page = page;
+    },
+  },
+};
