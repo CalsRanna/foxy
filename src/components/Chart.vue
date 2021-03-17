9b9349d748b3c58bc6f7374d842641d09cef12ae
CalsRanna <calsranna@gmail.com>
2021-03-18 00:42:13 +0800

    fix(reference_loot_template): fix render error

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -10,23 +10,24 @@ export default {
     return {
       ctx: null,
       chart: null,
-      timer: 0
+      timer: 0,
     };
   },
   props: {
     labels: {
       type: Array,
-      defaultValue: []
+      defaultValue: [],
     },
     data: {
       type: Array,
-      defaultValue: []
-    }
+      defaultValue: [],
+    },
   },
   watch: {
     data: function() {
-      (this.timer = setTimeout(this.renderChart(), 1000)), clearTimeout(this.timer);
-    }
+      (this.timer = setTimeout(this.renderChart(), 1000)),
+        clearTimeout(this.timer);
+    },
   },
   methods: {
     renderChart() {
@@ -43,7 +44,7 @@ export default {
                 "rgba(255, 206, 86, 0.8)",
                 "rgba(75, 192, 192, 0.8)",
                 "rgba(153, 102, 255, 0.8)",
-                "rgba(255, 159, 64, 0.8)"
+                "rgba(255, 159, 64, 0.8)",
               ],
               borderColor: [
                 "rgba(255, 99, 132, 1)",
@@ -51,18 +52,18 @@ export default {
                 "rgba(255, 206, 86, 1)",
                 "rgba(75, 192, 192, 1)",
                 "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)"
+                "rgba(255, 159, 64, 1)",
               ],
-              borderWidth: 1
-            }
-          ]
-        }
+              borderWidth: 1,
+            },
+          ],
+        },
       });
-    }
+    },
   },
   mounted() {
     this.ctx = document.getElementById("myChart").getContext("2d");
     this.renderChart();
-  }
+  },
 };
 </script>
