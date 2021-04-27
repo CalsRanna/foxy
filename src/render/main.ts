d4093046b0f82e3e3a3371daa4516c35a549bc9a
CalsRanna <calsranna@gmail.com>
2021-04-28 00:46:51 +0800

    chore(vite): replace webpack with vite

--- src/render/main.ts
+++ src/render/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
-import Home from './Home.vue'
+import App from '../App.vue'
+import router from '../router'
 
-createApp(Home).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount("#app")
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const config: UserConfig = {
   root,
   resolve: {
     alias: {
-      "/@": root,
+      "@": `${root}/src`,
     },
   },
   base: "./",
