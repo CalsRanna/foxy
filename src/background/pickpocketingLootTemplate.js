c11effc21ac23c081cc310847baec41513ca05df
CalsRanna <calsranna@gmail.com>
2021-05-27 19:17:07 +0800

    feat(pickpocketing loot template): add icon display in table

--- src/background/pickpocketingLootTemplate.js
+++ src/background/pickpocketingLootTemplate.js
@@ -13,7 +13,13 @@ import {
 
 ipcMain.on(SEARCH_PICKPOCKETING_LOOT_TEMPLATES, (event, payload) => {
   let queryBuilder = knex
-    .select(["plt.*", "it.name", "itl.Name as localeName"])
+    .select([
+      "plt.*",
+      "it.name",
+      "itl.Name as localeName",
+      "it.Quality",
+      "didi.InventoryIcon_1",
+    ])
     .from("pickpocketing_loot_template as plt")
     .leftJoin("item_template as it", "plt.Item", "it.entry")
     .leftJoin("item_template_locale as itl", function () {
@@ -23,6 +29,7 @@ ipcMain.on(SEARCH_PICKPOCKETING_LOOT_TEMPLATES, (event, payload) => {
         knex.raw("?", "zhCN")
       );
     })
+    .leftJoin("foxy.dbc_item_display_info as didi", "it.displayid", "didi.ID")
     .where("plt.Entry", payload.Entry);
 
   queryBuilder
