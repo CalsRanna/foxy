bb912ee05c14abaf463f95b0ca2f55e68f2a8505
CalsRanna <calsranna@gmail.com>
2022-03-18 12:28:55 +0800

    style(achievement): rename some label

---
+++ src/locales/achievement.js
@@ -0,0 +1,7 @@
+const factions = ["不限", "部落", "联盟"];
+
+const instanceIdTooltip = "成就和地图无关时设为-1。";
+
+const supercedesTooltip = "如果成就属于某一系列，设为上一个成就的ID。";
+
+export { factions, instanceIdTooltip, supercedesTooltip };
