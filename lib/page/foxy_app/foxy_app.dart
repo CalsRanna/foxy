c6a96a72eaef039bd1865eadd124fbcfddd6fb6e
Cals Ranna <calsranna@gmail.com>
2026-01-09 20:48:26 +0800

    feat(app): add platform-specific font family support for windows
    Set 'Microsoft YaHei' as default font family on Windows platforms while using system default for others. Consolidated theme data into a variable for better readability.

--- lib/page/foxy_app/foxy_app.dart
+++ lib/page/foxy_app/foxy_app.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:foxy/router/router.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
@@ -8,10 +10,16 @@ class FoxyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var shadSonnerTheme = ShadSonnerTheme(alignment: Alignment.topCenter);
+    var family = Platform.isWindows ? 'Microsoft YaHei' : null;
+    var shadTextTheme = ShadTextTheme(family: family);
+    var shadThemeData = ShadThemeData(
+      sonnerTheme: shadSonnerTheme,
+      textTheme: shadTextTheme,
+    );
     return ShadApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router.config(),
-      theme: ShadThemeData(sonnerTheme: shadSonnerTheme),
+      theme: shadThemeData,
     );
   }
 }
