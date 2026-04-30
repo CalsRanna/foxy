8f2f83bdd2f690df2242a9caec1c4ab6211d96e5
cals <calsmao@88vip.site>
2026-04-30 11:01:32 +0800

    refactor: reorder methods and rename preference variable

--- lib/util/window_initializer.dart
+++ lib/util/window_initializer.dart
@@ -12,20 +12,24 @@ class WindowInitializer with WindowListener {
   static const _defaultHeight = 750.0;
 
   static WindowInitializer? _instance;
-  late SharedPreferences _prefs;
+  late SharedPreferences _preference;
 
   WindowInitializer._();
 
-  static Future<void> ensureInitialized() async {
-    if (kIsWeb) return;
-    if (Platform.isAndroid || Platform.isFuchsia || Platform.isIOS) return;
+  @override
+  void onWindowResize() async {
+    final size = await windowManager.getSize();
+    await _saveWindowSize(size);
+  }
 
-    _instance = WindowInitializer._();
-    await _instance!._initialize();
+  Size _getSavedWindowSize() {
+    final width = _preference.getDouble(_keyWidth) ?? _defaultWidth;
+    final height = _preference.getDouble(_keyHeight) ?? _defaultHeight;
+    return Size(width, height);
   }
 
   Future<void> _initialize() async {
-    _prefs = await SharedPreferences.getInstance();
+    _preference = await SharedPreferences.getInstance();
     await windowManager.ensureInitialized();
 
     final savedSize = _getSavedWindowSize();
@@ -46,21 +50,23 @@ class WindowInitializer with WindowListener {
     windowManager.addListener(this);
   }
 
-  Size _getSavedWindowSize() {
-    final width = _prefs.getDouble(_keyWidth) ?? _defaultWidth;
-    final height = _prefs.getDouble(_keyHeight) ?? _defaultHeight;
-    return Size(width, height);
-  }
-
   Future<void> _saveWindowSize(Size size) async {
-    await _prefs.setDouble(_keyWidth, size.width);
-    await _prefs.setDouble(_keyHeight, size.height);
+    await _preference.setDouble(_keyWidth, size.width);
+    await _preference.setDouble(_keyHeight, size.height);
   }
 
-  @override
-  void onWindowResize() async {
-    final size = await windowManager.getSize();
-    await _saveWindowSize(size);
+  static Future<void> ensureInitialized() async {
+    if (kIsWeb) return;
+    if (Platform.isAndroid || Platform.isFuchsia || Platform.isIOS) return;
+
+    _instance = WindowInitializer._();
+    await _instance!._initialize();
+  }
+
+  static Future<void> opaque() async {
+    if (kIsWeb) return;
+    if (Platform.isAndroid || Platform.isFuchsia || Platform.isIOS) return;
+    await windowManager.setOpacity(1);
   }
 
   static Future<void> resize(Size size) async {
@@ -68,11 +74,5 @@ class WindowInitializer with WindowListener {
     if (Platform.isAndroid || Platform.isFuchsia || Platform.isIOS) return;
     await windowManager.setOpacity(0);
     await Future.wait([windowManager.setSize(size), windowManager.center()]);
-  }
-
-  static Future<void> opaque() async {
-    if (kIsWeb) return;
-    if (Platform.isAndroid || Platform.isFuchsia || Platform.isIOS) return;
-    await windowManager.setOpacity(1);
   }
 }
