b6cccfe65711605ee39669583a1cbd3932c4690e
Cals Ranna <calsranna@gmail.com>
2026-07-19 18:45:22 +0800

    refactor(cinematic-sequence): add typed row identity

--- lib/entity/cinematic_sequence_entity.dart
+++ lib/entity/cinematic_sequence_entity.dart
@@ -1,23 +1,3 @@
-class BriefCinematicSequenceEntity {
-  final int id;
-  final int soundId;
-  final int camera0;
-
-  const BriefCinematicSequenceEntity({
-    this.id = 0,
-    this.soundId = 0,
-    this.camera0 = 0,
-  });
-
-  factory BriefCinematicSequenceEntity.fromJson(Map<String, dynamic> json) {
-    return BriefCinematicSequenceEntity(
-      id: json['ID'] ?? 0,
-      soundId: json['SoundID'] ?? 0,
-      camera0: json['Camera0'] ?? 0,
-    );
-  }
-}
-
 class CinematicSequenceEntity {
   final int id;
   final int soundId;
