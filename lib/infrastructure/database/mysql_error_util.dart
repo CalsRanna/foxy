ccd4b8a60301dfa7ff901a91e5c5243ba0703b98
cals <calsmao@88vip.site>
2026-07-24 11:30:14 +0800

    refactor(mysql): detect duplicate entry from error message pattern
    Remove dependency on laconic_mysql's MySQLServerException class by parsing the error code from the LaconicException message string using a regex pattern. This change supports laconic_mysql v3.0.0 which no longer exports MySQLServerException. Also update related dependencies.

--- lib/infrastructure/database/mysql_error_util.dart
+++ lib/infrastructure/database/mysql_error_util.dart
@@ -1,15 +1,18 @@
 import 'package:laconic/laconic.dart';
-import 'package:laconic_mysql/laconic_mysql.dart';
 
 abstract final class MysqlErrorUtil {
   static const duplicateEntryCode = 1062;
+  static final _serverErrorCodePattern = RegExp(
+    r'^MysqlServerException \[(\d+)\]:',
+  );
 
   static bool isDuplicateEntry(Object error) {
-    if (error is MySQLServerException) {
-      return error.errorCode == duplicateEntryCode;
-    }
-    if (error is LaconicException && error.cause != null) {
-      return isDuplicateEntry(error.cause!);
+    for (Object? current = error; current is LaconicException;) {
+      final match = _serverErrorCodePattern.firstMatch(current.message);
+      if (match != null) {
+        return int.parse(match.group(1)!) == duplicateEntryCode;
+      }
+      current = current.cause;
     }
     return false;
   }
